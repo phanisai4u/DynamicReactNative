76499259912d945f6c401e3c2f7579229cf5059d
Phanisai <phanisai4u@gmail.com>
2023-06-07 15:53:49 +0530

    Create OnlinePage.tsx
    added sample online tsx file

---
+++ OnlinePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const OnlinePage: React.FC = () => {
+  return (
+    <View>
+      <Text>This is an online page downloaded from the internet.</Text>
+    </View>
+  );
+};
+
+export default OnlinePage;
